ae23b5e37e714bf5b26658a8784efa07a92aee22
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-22 22:09:12 +0530

    Add files via upload

---
+++ Soil_Moisture_Content_Based_Automatic_Motor_Pumping_for_Agriculture.ino
@@ -0,0 +1,41 @@
+int soilMoistureValue = 0;
+
+int percentage=0;
+
+void setup() {
+
+  pinMode(4,OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+soilMoistureValue = analogRead(A0);
+
+Serial.println(percentage);
+
+percentage = map(soilMoistureValue, 490, 1023, 100, 0);
+
+if(percentage < 10)  
+
+{
+
+  Serial.println(" pump on");
+
+  digitalWrite(4,LOW);
+
+}
+
+if(percentage >80)
+
+{
+
+  Serial.println("pump off");
+
+  digitalWrite(4,HIGH);
+
+}
+
+}
